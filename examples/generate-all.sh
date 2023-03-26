0ae590a22afd607cd7e65613b2bec9ca5cd2db08
anetczuk <anetczuk@o2.pl>
2023-03-26 01:22:28 +0100

    Add scripts

---
+++ examples/generate-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+
+## works both under bash and sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+
+$SCRIPT_DIR/simple/generate.py
+
+
+## generate small images
+$SCRIPT_DIR/generate_small.sh
