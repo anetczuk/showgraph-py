3c83bc59695fe9fc6b083ff84cebbfef4b6b418d
anetczuk <anetczuk@o2.pl>
2023-03-27 00:31:43 +0200

    Install data file

--- src/setup.py
+++ src/setup.py
@@ -4,23 +4,18 @@ import os
 
 from setuptools import setup, find_packages
 
+from showgraph.io import read_list
+
 
 SCRIPT_DIR = os.path.dirname( os.path.abspath(__file__) )
 
 
-def read_list( file_path ):
-    if not os.path.isfile( file_path ):
-        return []
-    ret_list = []
-    with open( file_path, 'r', encoding='utf-8' ) as content_file:
-        for line in content_file:
-            ret_list.append( line.strip() )
-    return ret_list
-
-
 packages_list = find_packages( include=['showgraph', 'showgraph.*'] )
 
-install_reqs = read_list( os.path.join( SCRIPT_DIR, "requirements.txt" ) )
+data_files = [ ( "showgraph", ["graphviz_dark_colors.data.txt"] ) ]
+
+requirements_path = os.path.join( SCRIPT_DIR, "requirements.txt" )
+install_reqs  = read_list( requirements_path )
 
 ## every time setup info changes then version number should be increased
 
@@ -31,5 +26,6 @@ setup( name='showgraph',
        author="anetczuk",
        license="BSD 3-Clause License",
        packages=packages_list,
+       data_files=data_files,
        install_requires=install_reqs
        )
